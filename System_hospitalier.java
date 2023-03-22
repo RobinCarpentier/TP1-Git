4d701b70b9bdfd0e6ba76be26c84f88f6b02811c
Robin <robin.carpentier@free.fr>
2023-03-22 14:55:57 +0100

    Fichier servant pour le système hospitalier

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -34,12 +34,12 @@ public class System_hospitalier {
 	}
 	
 	private String analyserRequete(Requête requête) {
-		// Analyser la requête
+		// Analyser la requête en retournant "requête analysée"
 		return "requête analysée";
 	}
 	
 	private String comparerDonnes() {
-		// Comparaison des données
+		// Comparaison des données en retournant "Cluster trouvé"
 		return "Cluster trouvé";
 	}
 
